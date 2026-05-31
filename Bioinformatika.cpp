cb98dae039b2f83d59a2db61891ca997f6639c13
Ema Adamec <ema.adamec@fer.hr>
2026-05-31 23:45:30 +0200

    Final editing

--- Bioinformatika.cpp
+++ Bioinformatika.cpp
@@ -36,175 +36,49 @@ int getHammingDistance(const string& sequence1, const string& sequence2){
     return distance;
 }
 
-int main() {
-
-    /*Definiranje strukture ulaza (ulaz su .fastaq datoteke), granice za ulaz u klaster i liste klastera*/
-    string header, NucleotideSequence, plus, qualityScore;
-    int threshold = 12;
-    
-    vector<Cluster> allClusters;
-    vector<Read> allRepresentatives;
-
-    /* Load all J*.fastq files */
-    for (const auto& entry : fs::directory_iterator("./fastq")) {
-
-        string filename = entry.path().filename().string();
-
-        /* Only process files starting with J */
-        if (filename[0] == 'J') {
-
-            cout << "Loading file: " << filename << endl;
-
-            ifstream file(entry.path());
-
-            vector<Cluster> myClusters;
-            vector<Read> Reads; 
-
-
-            while (
-                getline(file, header) &&
-                getline(file, NucleotideSequence) &&
-                getline(file, plus) &&
-                getline(file, qualityScore)
-            ) {
-
-                if (NucleotideSequence.length() == 296) {
-
-                    Reads.push_back({
-                        filename + " | " + header,
-                        NucleotideSequence,
-                        ""
-                    });
-                }
-            }
-
-            /* Generiranje MSA*/
-
-            auto alignment_engine = spoa::AlignmentEngine::Create(spoa::AlignmentType::kNW, 0, -1, -1);  
-
-            spoa::Graph graph{};
-
-            for (const auto& it : Reads) {
-                auto alignment = alignment_engine->Align(it.originalSequence, graph);
-                graph.AddAlignment(alignment, it.originalSequence);
-            }
-
-            auto msa = graph.GenerateMultipleSequenceAlignment();
-
-            for (size_t i = 0; i < Reads.size(); i++) {
-                Reads[i].MSA_Sequence = msa[i];
-            }
-
-            /* Klasteriranje */
-
-            for (const Read& r : Reads) {
-
-                if (myClusters.empty()) {
-                    Cluster newCluster;
-                    newCluster.sequences.push_back(r);
-                    myClusters.push_back(newCluster);
-                    continue;
-                }
-
-                int bestClusterIndex = -1;
-                int bestDistance = INT_MAX;
-
-                for (int i = 0; i < myClusters.size(); i++) {
-
-                    int clusterMin = INT_MAX;
-
-                    for (const Read& s : myClusters[i].sequences) {
-                        int dist = getHammingDistance(s.MSA_Sequence, r.MSA_Sequence);
-                        clusterMin = min(clusterMin, dist);
-                    }
-
-                    if (clusterMin < bestDistance) {
-                        bestDistance = clusterMin;
-                        bestClusterIndex = i;
-                    }
-                }
-
-                if (bestDistance <= threshold) {
-                    myClusters[bestClusterIndex].sequences.push_back(r);
-                } else {
-                    Cluster newCluster;
-                    newCluster.sequences.push_back(r);
-                    myClusters.push_back(newCluster);
-                }
-            }
-
-            myClusters.erase(
-                remove_if(
-                    myClusters.begin(),
-                    myClusters.end(),
-                        [](const Cluster& c) {
-                        return c.sequences.size() <= 2;
-                    }
-                ),
-                    myClusters.end()
-            );
-
-            for (Cluster& c : myClusters) {
-
-                auto alignment_engine = spoa::AlignmentEngine::Create(
-                    spoa::AlignmentType::kNW, 0, -1, -1
-                );
-
-                spoa::Graph graph;
-
-                for (const Read& r : c.sequences) {
-                    auto alignment = alignment_engine->Align(r.originalSequence, graph);
-                    graph.AddAlignment(alignment, r.originalSequence);
-                }
-
-                auto consensus = graph.GenerateConsensus();
-                c.representativeSequence = consensus;
-            }
-
-            for (Cluster& c : myClusters) {
-                Read newRepresentative;
-                newRepresentative.originalSequence = c.representativeSequence;
-                allRepresentatives.push_back(newRepresentative);
-            }
-        }
-    }
-
-    /* Generiranje MSA*/
-
+/*Generiranje MSA*/
+void GenerateMSA(vector<Read> &Reads){
     auto alignment_engine = spoa::AlignmentEngine::Create(spoa::AlignmentType::kNW, 0, -1, -1);  
 
     spoa::Graph graph{};
 
-    for (const auto& it : allRepresentatives) {
+    for (const auto& it : Reads) {
         auto alignment = alignment_engine->Align(it.originalSequence, graph);
         graph.AddAlignment(alignment, it.originalSequence);
     }
 
     auto msa = graph.GenerateMultipleSequenceAlignment();
 
-    for (size_t i = 0; i < allRepresentatives.size(); i++) {
-        allRepresentatives[i].MSA_Sequence = msa[i];
+    for (size_t i = 0; i < Reads.size(); i++) {
+        Reads[i].MSA_Sequence = msa[i];
     }
 
-    /* Klasteriranje */
+    return;
+}
 
-    for (const Read& r : allRepresentatives) {
+/*Klasteriranje - prolazimo po klasterima i svim njihovim trenutnim članovima (sekvencama) kako bi našli onu s najmanjom udaljnosti i zatim 
+sekvencu dodajemo u klaster u kojem se nalazi sekvenca s najmanjom razlikom u udaljenosti s time da ne smije prelaziti threshold*/
+vector<Cluster> Clustering(vector<Read> &Reads, int threshold){
 
-        if (allClusters.empty()) {
+    vector<Cluster> Clusters;
+
+    for (const Read& r : Reads) {
+
+        if (Clusters.empty()) {
             Cluster newCluster;
             newCluster.sequences.push_back(r);
-            allClusters.push_back(newCluster);
+            Clusters.push_back(newCluster);
             continue;
         }
 
         int bestClusterIndex = -1;
         int bestDistance = INT_MAX;
 
-        for (int i = 0; i < allClusters.size(); i++) {
+        for (int i = 0; i < Clusters.size(); i++) {
 
             int clusterMin = INT_MAX;
 
-            for (const Read& s : allClusters[i].sequences) {
+            for (const Read& s : Clusters[i].sequences) {
                 int dist = getHammingDistance(s.MSA_Sequence, r.MSA_Sequence);
                 clusterMin = min(clusterMin, dist);
             }
@@ -216,15 +90,20 @@ int main() {
         }
 
         if (bestDistance <= threshold) {
-            allClusters[bestClusterIndex].sequences.push_back(r);
+            Clusters[bestClusterIndex].sequences.push_back(r);
         } else {
             Cluster newCluster;
             newCluster.sequences.push_back(r);
-            allClusters.push_back(newCluster);
+            Clusters.push_back(newCluster);
         }
     }
+    
+    return Clusters;
+}
 
-    for (Cluster& c : allClusters) {
+/*Generiranje konsenzusne sekvence*/
+void GenerateConsensus(vector<Cluster> &clusters){
+    for (Cluster& c : clusters) {
 
         auto alignment_engine = spoa::AlignmentEngine::Create(
             spoa::AlignmentType::kNW, 0, -1, -1
@@ -240,14 +119,189 @@ int main() {
         auto consensus = graph.GenerateConsensus();
         c.representativeSequence = consensus;
     }
+}
 
-    /*Ispis rezultata*/
+/*Učitavanje podataka*/
+vector<Read> LoadReads(const fs::path& filepath){
+    vector<Read> reads;
 
-    cout << "Clusters: " << allClusters.size() << endl;
+    string header, sequence, plus, quality;
 
-    for (Cluster &c : allClusters){
-        cout << "Cluster size: " << c.sequences.size() << endl;
-        cout << "Representative: " << c.representativeSequence << endl << endl;
+    ifstream file(filepath);
+
+    /*Učitavamo po četiri retka odjednom naziv očitanja, sekvencu, znak plus i 'quality score' iz .fastaq datoteke*/
+    while (
+        getline(file, header) &&
+        getline(file, sequence) &&
+        getline(file, plus) &&
+        getline(file, quality)
+    ) {
+        if (sequence.length() == 296) {
+            reads.push_back({
+                filepath.filename().string() + " | " + header,
+                sequence,
+                ""
+            });
+        }
+    }
+
+    return reads;
+}
+
+/* Glavni program */
+
+int main() {
+
+    string inputPath;
+
+    cout << "Enter FASTQ file or folder: ";
+    getline(cin, inputPath);
+
+    bool singleFile = false;
+
+    if (fs::is_regular_file(inputPath)) {
+        singleFile = true;
+    } else if (!fs::is_directory(inputPath)) {
+        cerr << "Invalid path!" << endl;
+        return 1;
+    }
+
+    /*Definiranje granice za ulaz u klaster*/
+    int threshold = 12;
+
+    /*Način rada za samo jednu datoteku*/
+    if (singleFile){
+        
+        ifstream file(inputPath);
+
+        vector<Read> Reads = LoadReads(inputPath);
+
+        GenerateMSA(Reads);
+        vector<Cluster> myClusters = Clustering(Reads, threshold);
+        
+        /*Izbacujemo male klastere*/
+        myClusters.erase(
+            remove_if(
+                    myClusters.begin(),
+                    myClusters.end(),
+                        [](const Cluster& c) {
+                        return c.sequences.size() <= 2;
+                    }
+                ),
+                    myClusters.end()
+        );
+
+        GenerateConsensus(myClusters);    
+
+        /*Ispis rezultata u datoteku u FASTA formatu i membership.txt*/
+        ofstream fasta(fs::path(inputPath).stem().string() + "_variants.fasta");
+
+        for (size_t i = 0; i < myClusters.size(); i++) {
+
+            fasta << ">Variant_" << i + 1
+                << "_size_" << myClusters[i].sequences.size()
+                << "\n";
+
+            fasta << myClusters[i].representativeSequence
+                << "\n";
+        }
+
+        ofstream clusters(fs::path(inputPath).stem().string() + "_cluster_membership.txt");
+
+        for (size_t i = 0; i < myClusters.size(); i++) {
+
+            clusters << "Cluster "
+                    << i + 1
+                    << "\n";
+
+            clusters << "Representative:\n";
+            clusters << myClusters[i].representativeSequence
+                    << "\n\n";
+
+            clusters << "Reads:\n";
+
+            for (const Read& r : myClusters[i].sequences) {
+                clusters << r.name << "\n";
+            }
+
+            clusters << "\n--------------------------------\n\n";
+        }
+
+    /*Način rada da folder s datotekama*/
+    } else {
+
+        vector<Cluster> allClusters;
+        vector<Read> allRepresentatives;
+
+        for (const auto& entry : fs::directory_iterator(inputPath)){
+
+            cout << "Loading file: " << entry.path().filename().string() << endl;
+
+            vector<Read> Reads = LoadReads(entry.path());
+
+            GenerateMSA(Reads);
+            vector<Cluster> myClusters = Clustering(Reads, threshold);
+
+            myClusters.erase(
+                remove_if(
+                    myClusters.begin(),
+                    myClusters.end(),
+                        [](const Cluster& c) {
+                        return c.sequences.size() <= 2;
+                    }
+                ),
+                    myClusters.end()
+            );
+
+            GenerateConsensus(myClusters);
+
+            for (Cluster& c : myClusters) {
+                Read newRepresentative;
+                newRepresentative.name = "Variant_representative_of_" + entry.path().filename().string();
+                newRepresentative.originalSequence = c.representativeSequence;
+                allRepresentatives.push_back(newRepresentative);
+            }
+        }
+
+        /*Klasteriranje dobivenih varijanti iz svakog uzorka*/
+        GenerateMSA(allRepresentatives);
+
+        allClusters = Clustering(allRepresentatives, threshold);
+        GenerateConsensus(allClusters);
+
+        ofstream fasta("Variants.fasta");
+
+        for (size_t i = 0; i < allClusters.size(); i++) {
+
+            fasta << ">Variant_" << i + 1
+                << "_size_" << allClusters[i].sequences.size()
+                << "\n";
+
+            fasta << allClusters[i].representativeSequence
+                << "\n";
+        }
+
+        ofstream clusters("Cluster_membership.txt");
+
+        for (size_t i = 0; i < allClusters.size(); i++) {
+
+            clusters << "Cluster "
+                    << i + 1
+                    << "\n";
+
+            clusters << "Representative:\n";
+            clusters << allClusters[i].representativeSequence
+                    << "\n\n";
+
+            clusters << "Reads:\n";
+
+            for (const Read& r : allClusters[i].sequences) {
+                clusters << r.name << "\n";
+            }
+
+            clusters << "\n--------------------------------\n\n";
+        }
+
     }
 
     return 0;
